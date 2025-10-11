e7d9396cf4011d9eb33404871890cd6f2be88b9e
Artem Grachov <grachov2195@gmail.com>
2025-10-11 16:05:12 +0300

    refactor: modal header, ECD-8

--- src/components/modal/ModalNav/index.tsx
+++ src/components/modal/ModalNav/index.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { ComponentType } from 'react';
-import { X } from 'react-bootstrap-icons';
 
 import Nav from '@/components/nav/Nav';
 import ModalFullscreen from '@/components/modal/ModalFullscreen';
@@ -9,17 +8,12 @@ import ModalFullscreen from '@/components/modal/ModalFullscreen';
 import type { IModalProps } from '@/providers/modals/types';
 
 import styles from './styles.module.scss';
+import ModalHeader from '@/components/modal/ModalHeader';
 
 const ModalNav: ComponentType<IModalProps> = (props) => {
   return (
     <ModalFullscreen {...props}>
-      <header className={styles.header}>
-        <div className={styles.container}>
-          <button type="button" className={styles.close} onClick={props.close}>
-            <X size={42} />
-          </button>
-        </div>
-      </header>
+      <ModalHeader {...props} />
       <div className={styles.content}>
         <Nav />
       </div>
