a4056f27a2c6b394a1b57fdf311645f6027b6a08
Artem Grachov <grachov2195@gmail.com>
2025-10-13 00:05:10 +0300

    feat: cart counter and placeholder, ECD-4

--- src/components/cart/CartToggle/index.tsx
+++ src/components/cart/CartToggle/index.tsx
@@ -1,48 +1,27 @@
 'use client';
 
-import { ComponentType, lazy, PropsWithChildren } from 'react';
+import { ComponentType } from 'react';
 import { Cart } from 'react-bootstrap-icons';
-import { useScreenSize } from 'react-screen-size-helper';
-
-import { BREAKPOINTS } from '@/constants/screen';
-
-import { useModalsCtx } from '@/providers/modals/hooks/use-modals-ctx';
 
 import { ROUTES } from '@/router/routes';
 
 import { useRoutePath } from '@/hooks/routing/use-route-path';
+import { useCartItems } from '@/hooks/cart/cart-items';
 import IconButton from '@/components/buttons/IconButton';
 
-const ModalCart = lazy(() => import('@/components/modal/ModalCart'));
+import styles from './styles.module.scss';
 
 const CartToggle: ComponentType = () => {
-  const { openModal } = useModalsCtx();
-  const { isMobile, isTablet } = useScreenSize({ breakpoints: BREAKPOINTS });
   const routePath = useRoutePath();
+  const cartItems = useCartItems();
+  const cartItemsCount = cartItems.reduce((acc, curr) => acc + curr.quantity, 0);
 
-  const openCartHandler = <T,>() => {
-    openModal({ id: 'MODAL_CART', component: ModalCart });
-  }
-
-  const CartButton = ({ children }: PropsWithChildren) => (
-    <IconButton type="button" onClick={openCartHandler} tag={'button'}>
-      {children}
+  return (
+    <IconButton href={routePath(ROUTES.CART)} tag={'Link'} className={styles.cartToggle}>
+      <Cart size={42} />
+      {cartItemsCount ? <span className={styles.counter}>{cartItemsCount}</span> : null}
     </IconButton>
   )
-
-  const CartLink = ({ children }: PropsWithChildren) => (
-    <IconButton href={routePath(ROUTES.CART)} tag={'Link'}>
-      {children}
-    </IconButton>
-  )
-
-  const Component = (isMobile || isTablet) ? CartButton : CartLink;
-
-return (
-  <Component>
-    <Cart size={42} />
-  </Component>
-)
 }
 
 export default CartToggle;
