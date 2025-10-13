5810b0530c0bcae994cc503955d2408eacca443d
Artem Grachov <grachov2195@gmail.com>
2025-10-14 01:13:26 +0300

    feat: gallery zoom, ECD-2

--- src/components/media/Gallery/index.tsx
+++ src/components/media/Gallery/index.tsx
@@ -1,12 +1,16 @@
 'use client'
 
-import { ComponentType } from 'react';
+import { ComponentType, useEffect } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import InnerImageZoom from 'inner-image-zoom';
+import clsx from 'clsx';
+import { Search } from 'react-bootstrap-icons';
 
 import type { IPropsWithClassName } from '@/types/other/component-props';
 import type { IMedia } from '@/types/models/media';
 
 import 'swiper/css';
+import 'inner-image-zoom/lib/styles.min.css';
 import styles from './styles.module.scss';
 
 interface IProps {
@@ -16,8 +20,20 @@ interface IProps {
 const Gallery: ComponentType<IPropsWithClassName & IProps> = ({ className }) => {
   const media = [1, 2, 3, 4, 5];
 
+  const initZoom = () => {
+    new InnerImageZoom(`.${styles.image}`, {
+      zoomType: 'hover',
+      hideCloseButton: true,
+      hideHint: true,
+    });
+  }
+
+  useEffect(() => {
+    initZoom();
+  }, []);
+
   return (
-    <div className={className}>
+    <div className={clsx(styles.gallery, className)}>
       <Swiper
         className={styles.swiper}
         spaceBetween={50}
@@ -25,11 +41,14 @@ const Gallery: ComponentType<IPropsWithClassName & IProps> = ({ className }) => 
         {media.map((m, i) => {
           return (
             <SwiperSlide key={i} className={styles.slide}>
-              <img src="https://placehold.co/600x400" alt={m.toString()} className={styles.image} />
+              <img src="https://placehold.co/1600x800" alt={m.toString()} className={styles.image} />
             </SwiperSlide>
           )
         })}
       </Swiper>
+      <div className={styles.zoomIcon}>
+        <Search size={'100%'} />
+      </div>
     </div>
   )
 }
