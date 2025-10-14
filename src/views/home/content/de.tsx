738fd26e2079dea13a3ea3172f512cea66fe4010
Artem Grachov <grachov2195@gmail.com>
2025-10-14 04:44:29 +0300

    feat: home page translation, ECD-1

--- src/views/home/content/de.tsx
+++ src/views/home/content/de.tsx
@@ -4,7 +4,7 @@ const ContentDE: ComponentType = () => {
   return (
     <>
       <h1>
-        Demo-Next.js-E-Commerce-Website
+        Demo E-Commerce-Website mit Next.js
       </h1>
       <p>
         Diese Website wurde zu Demonstrationszwecken entwickelt und dient als Beispiel für eine auf Next.js basierende E-Commerce-Website.
@@ -23,7 +23,7 @@ const ContentDE: ComponentType = () => {
               Produktkatalog nach Kategorien
             </li>
             <li>
-              Suche in allen Produkten
+              Suche unter allen Produkten
             </li>
             <li>
               Suche innerhalb einer Kategorie
@@ -45,7 +45,7 @@ const ContentDE: ComponentType = () => {
           </ul>
         </li>
         <li>
-          Bestellvorgang, einschließlich Weiterleitung zu einer pseudoexternen Website
+          Bestellvorgang einschließlich einer Umleitung auf eine pseudo-externe Website
         </li>
         <li>
           Helles und dunkles Design
@@ -63,6 +63,64 @@ const ContentDE: ComponentType = () => {
       <p>
         Dieses Projekt ist Open Source. Sie können es frei und ohne Genehmigung verwenden.
       </p>
+      <h2>
+        Verwendete Medien
+      </h2>
+      <p>
+        Sämtliche Medieninhalte sind kostenlos und stammen von <a href="https://unsplash.com/">Unsplash</a>.
+      </p>
+      <ul>
+        <li>
+          <a href="https://unsplash.com/photos/a-white-cell-phone-sitting-on-top-of-a-pink-and-blue-background-0ej0jMwPFhQ">
+            https://unsplash.com/photos/a-white-cell-phone-sitting-on-top-of-a-pink-and-blue-background-0ej0jMwPFhQ
+          </a>
+        </li>
+        <li>
+          <a href="https://unsplash.com/photos/white-smartphone-near-laptop-xsGxhtAsfSA">
+            https://unsplash.com/photos/white-smartphone-near-laptop-xsGxhtAsfSA
+          </a>
+        </li>
+        <li>
+          <a href="https://unsplash.com/photos/a-laptop-computer-sitting-on-top-of-a-wooden-table-6RqSDGaNJ5c">
+            https://unsplash.com/photos/a-laptop-computer-sitting-on-top-of-a-wooden-table-6RqSDGaNJ5c
+          </a>
+        </li>
+        <li>
+          <a href="https://unsplash.com/photos/a-laptop-computer-sitting-on-top-of-a-desk-fc1dt-T0JMI">
+            https://unsplash.com/photos/a-laptop-computer-sitting-on-top-of-a-desk-fc1dt-T0JMI
+          </a>
+        </li>
+        <li>
+          <a href="https://unsplash.com/photos/gold-beats-wireless-headphones-bWZAPKm0zZE">
+            https://unsplash.com/photos/gold-beats-wireless-headphones-bWZAPKm0zZE
+          </a>
+        </li>
+        <li>
+          <a href="https://unsplash.com/photos/white-sony-corded-headphones-RZmiDOpv1lM">
+            https://unsplash.com/photos/white-sony-corded-headphones-RZmiDOpv1lM
+          </a>
+        </li>
+        <li>
+          <a href="https://unsplash.com/photos/a-tablet-computer-sitting-on-top-of-a-table-0cG_yQAdYIM">
+            https://unsplash.com/photos/a-tablet-computer-sitting-on-top-of-a-table-0cG_yQAdYIM
+          </a>
+        </li>
+        <li>
+          <a href="https://unsplash.com/photos/black-g208-smart-watch-QhF3YGsDrYk">
+            https://unsplash.com/photos/black-g208-smart-watch-QhF3YGsDrYk
+          </a>
+        </li>
+        <li>
+          <a href="https://unsplash.com/photos/red-xbox-one-game-controller-hUD0PUczwJQ">
+            https://unsplash.com/photos/red-xbox-one-game-controller-hUD0PUczwJQ
+          </a>
+        </li>
+        <li>
+          <a href="https://unsplash.com/photos/a-room-with-a-plant-and-a-thermostaer-on-the-wall-LUFaSAGhk1c">
+            https://unsplash.com/photos/a-room-with-a-plant-and-a-thermostaer-on-the-wall-LUFaSAGhk1c
+          </a>
+        </li>
+      </ul>
     </>
   )
 }
