027a2ac66ac290f3324054c7658adce47f0f683a
Artem Grachov <grachov2195@gmail.com>
2025-10-14 04:00:50 +0300

    feat: landing, ECD-7

---
+++ src/views/home/content/de.tsx
@@ -0,0 +1,70 @@
+import { ComponentType } from 'react';
+
+const ContentDE: ComponentType = () => {
+  return (
+    <>
+      <h1>
+        Demo-Next.js-E-Commerce-Website
+      </h1>
+      <p>
+        Diese Website wurde zu Demonstrationszwecken entwickelt und dient als Beispiel für eine auf Next.js basierende E-Commerce-Website.
+      </p>
+      <h2>
+        Funktionen
+      </h2>
+      <ul>
+        <li>
+          Produktkatalog:
+          <ul>
+            <li>
+              Allgemeiner Produktkatalog
+            </li>
+            <li>
+              Produktkatalog nach Kategorien
+            </li>
+            <li>
+              Suche in allen Produkten
+            </li>
+            <li>
+              Suche innerhalb einer Kategorie
+            </li>
+            <li>
+              Filterung nach Preis
+            </li>
+          </ul>
+        </li>
+        <li>
+          Produktseite
+          <ul>
+            <li>
+              Mediengalerie mit Zoom-Funktion
+            </li>
+            <li>
+              Bewertungen
+            </li>
+          </ul>
+        </li>
+        <li>
+          Bestellvorgang, einschließlich Weiterleitung zu einer pseudoexternen Website
+        </li>
+        <li>
+          Helles und dunkles Design
+        </li>
+        <li>
+          Internationalisierung
+        </li>
+        <li>
+          Mobile-First-Markup
+        </li>
+      </ul>
+      <h2>
+        Verwendung
+      </h2>
+      <p>
+        Dieses Projekt ist Open Source. Sie können es frei und ohne Genehmigung verwenden.
+      </p>
+    </>
+  )
+}
+
+export default ContentDE;
--- src/views/home/view.tsx
+++ src/views/home/view.tsx
@@ -1,12 +1,30 @@
+import { getLocale } from 'next-intl/server';
+
+import ContentEN from './content/en';
+import ContentDE from './content/de';
+
 import styles from './styles.module.scss';
 
-const HomeView = () => {
+const HomeView = async () => {
+  const locale = await getLocale();
+
+  let content;
+
+  switch (locale) {
+    case 'en': {
+      content = <ContentEN />;
+      break;
+    }
+    case 'de': {
+      content = <ContentDE />;
+      break;
+    }
+  }
+
   return (
     <main className={styles.page}>
       <div className={styles.container}>
-        <h1>
-          Demo Next.js E-Commerce website
-        </h1>
+        {content}
       </div>
     </main>
   )
