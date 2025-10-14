fa198fcfaf0e8fe8ab0b8b92b155e665606153a7
Artem Grachov <grachov2195@gmail.com>
2025-10-14 10:59:52 +0300

    feat: cart page skeletons, ECD-4

--- src/providers/cart/store/types.ts
+++ src/providers/cart/store/types.ts
@@ -3,17 +3,21 @@ import type { IOrder } from '@/types/models/order';
 
 export type State = {
   order: IOrder | null;
+  isInitialized: boolean;
 }
 
 export const enum EActions {
   SET,
+  SET_INITIALIZED,
 }
+
+export interface ISetInitialized extends IReducerAction<EActions.SET_INITIALIZED> { }
 
 export interface ISetAction extends IReducerAction<EActions.SET> {
   order: IOrder | null;
 }
 
-export type Action = ISetAction;
+export type Action = ISetAction | ISetInitialized;
 
 export type StoreActions = {
   dispatch: Dispatch<Action>;
